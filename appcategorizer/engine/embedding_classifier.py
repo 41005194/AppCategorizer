4ee0aa258269c330cf52a0bd740a1147d9390161
41005194 <41005194@parisnanterre.fr>
2026-06-18 16:55:26 +0200

    Add copyrights to all  Python files

--- appcategorizer/engine/embedding_classifier.py
+++ appcategorizer/engine/embedding_classifier.py
@@ -1,3 +1,10 @@
+# Copyright (c) 2026, Sorbonne Université, CNRS, LIP6.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the
+# GNU Lesser General Public License v3.0 (LGPL-3.0-only)
+# which accompanies this distribution, and is available at
+# https://www.gnu.org/licenses/lgpl-3.0.en.html
+
 import os
 from pathlib import Path
 import re
